0b1c7f1f55b4132b4250999395822b3cde0f89e6
dfish19 <dfish19@yahoo.com>
2022-09-09 16:58:20 -0500

    initial

--- APIExercise2/Program.cs
+++ APIExercise2/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using APIExercise2;
+using Newtonsoft.Json.Linq;
+
+var key = File.ReadAllText("mapapp.json");
+var APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+Console.WriteLine("Enter name of a city");
+var cityName = Console.ReadLine();
+var apiCall = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=imperial&appid={APIKey}";
+Console.WriteLine();
+
+
+Console.WriteLine($"The temperature in {cityName} is {WeatherMap.Temp(apiCall)}");
